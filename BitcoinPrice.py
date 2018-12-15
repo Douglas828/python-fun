4791871de12e8c0c678542f7dd78a79680821af2
TheGamerDoug <douglasdenning89@gmail.com>
2018-12-14 20:32:39 -0500

    Intial Commit - Added BTC Price Checker

---
+++ BitcoinPrice.py
@@ -0,0 +1,91 @@
+import urllib2
+import json
+import pprint
+import sched, time
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+# Variables
+
+percentDifference = 1
+hasStarted = 0
+value = str(0)
+valueFloat = float(value)
+updatedValue = str(value)
+updatedValueFloat = float(updatedValue)
+
+# Get Coin
+	
+coin = raw_input('Enter the coin ticker: ')
+	
+# Make Capital
+	
+coinT = coin.upper() 
+
+# Tell user what they chose
+
+print('You chose: ' + coinT) 
+
+def valueFunc() :
+			global hasStarted
+			global value
+			global valueFloat
+			global updatedValue
+			global updatedValueFloat
+			if(hasStarted == 0) :
+				
+				base = urllib2.urlopen('https://min-api.cryptocompare.com/data/price?fsym=' + coinT + '&tsyms=USD')
+				data = json.load(base)
+
+				value = str(data["USD"])
+				valueFloat = float(value)
+				time.sleep(5)
+				
+				print(coinT + ' Starting Value: ' + value)
+				
+				hasStarted = hasStarted + 1
+				
+		
+			base = urllib2.urlopen('https://min-api.cryptocompare.com/data/price?fsym=' + coinT + '&tsyms=USD')
+			data = json.load(base)
+			global updatedValue
+			global updatedValueFloat
+			updatedValue = str(data["USD"])   
+			updatedValueFloat = float(updatedValue)
+				
+			percentDifference = round(abs(float((((valueFloat - updatedValueFloat) / valueFloat) * 100))), 2)
+			
+			# if(percentDifference < 0.05) :
+				
+				
+				
+			# print(str(percentDifference))
+			# percentNumber = percentDifference * 10
+			print(coinT + ' Value: $' + updatedValue)
+			print('Value Changed: ' + str((percentDifference)) + '%')
+			log = open(coinT + "_price_change.txt","a")
+			log.write(updatedValue + " : ")
+			log.write(datetime.datetime.now + "\n")
+			value = updatedValue
+			valueFloat = updatedValueFloat
+			time.sleep(10)
+				
+
+
+
+def plot() :
+	global hasStarted
+	global value
+	global valueFloat
+	global updatedValue
+	global updatedValueFloat
+	
+	
+
+
+while (abs(percentDifference) >= 0) :
+	valueFunc()
+
+
+
+
